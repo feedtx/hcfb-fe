75c4e62b36a81404598311b402aa7c327ee3e81e
BenjaminWalkerBond <bwb63@txstate.edu>
2020-01-31 19:48:33 -0600

    Created the launch page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
+
+</style>
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="d-flex justify-content-center" style="margin-top: 3em; margin-bottom: 2em;">
+    <h1>[feed]</h1>
   </div>
+  <div class="d-flex justify-content-center">
+    <button type="button" class="btn btn-menu"><i class="fas fa-apple-alt" style="font-size: 8em; color: #8067B7">
+    </i><br><br>
+    Food Rescue</button>
+    <button  type="button" class="btn btn-menu weight"><i class="fas fa-weight" style="font-size: 8em; color: #7DB1B1">
+    </i><br><br>
+    Warehouse Entry </button>
+  </div>
+  <div class="d-flex justify-content-center">
+    <button  type="button" class="btn btn-menu"><i class="fas fa-truck" style="font-size: 8em; background-color:">
+    </i><br><br>
+    Distribution </button>
+    <button  type="button" class="btn btn-menu"><i class="fas fa-tools" style="font-size: 8em">
+    </i><br><br>
+    Admin </button>
+  </div>
+      
+      
+    </div>
 </template>
 
 <script>
@@ -11,6 +32,7 @@ export default {
 }
 </script>
 
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -18,6 +40,32 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #282c34
 }
+body {
+    background-color: #282c34;
+}
+
+h1 {
+	font-family: 'Open Sans', serif;
+	font-size: 6em;
+	color: white;
+} 
+
+.btn-menu {
+	margin-left: 2rem;
+	margin-right: 2rem;
+	width: 15rem;
+	height: 15rem;
+	margin-top: 1rem;
+	margin-bottom: 1rem;
+	font-family: 'Open Sans', sans-serif;
+	font-weight: bold;
+	color: #ffffff;
+}
+.weight {
+  background:url("/Users/michaelrwalker/Documents/GitHub/hcfb-fe/src/assets");
+}
+
 </style>
+
