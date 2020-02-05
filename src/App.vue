512a645711c560ef71a2b8cb58d896154e1be324
Justice-Micka <citiesinspace@protonmail.com>
2020-02-05 17:18:09 -0600

    Ported landing page over to Vue component and updated Vue router accordingly, modified files to remove/add CSS styling. Added a noscript tag in the index.html

--- src/App.vue
+++ src/App.vue
@@ -2,69 +2,31 @@
 </style>
 <template>
   <div id="app">
-    <div class="d-flex justify-content-center" style="margin-top: 3em; margin-bottom: 2em;">
-    <h1>[feed]</h1>
-  </div>
-  <div class="d-flex justify-content-center">
-    <button type="button" class="btn btn-menu"><i class="fas fa-apple-alt" style="font-size: 8em; color: #EE0055">
-    </i><br><br>
-    Food Rescue</button>
-    <button  type="button" class="btn btn-menu weight"><i class="fas fa-weight" style="font-size: 8em; color: #7DB1B1">
-    </i><br><br>
-    Warehouse Entry </button>
-  </div>
-  <div class="d-flex justify-content-center">
-    <button  type="button" class="btn btn-menu"><i class="fas fa-truck" style="font-size: 8em; color: #0e6502">
-    </i><br><br>
-    Distribution </button>
-    <button  type="button" class="btn btn-menu"><i class="fas fa-tools" style="font-size: 8em; color: #2c33CC">
-    </i><br><br>
-    Admin </button>
-  </div>
+    <router-view></router-view>
       
-      
-    </div>
+  </div>
 </template>
 
 <script>
+import LandingMenu from "@/components/LandingMenu";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    LandingMenu
+  }
 };
 </script>
 
 
 <style>
-#app {
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   background-color: #282c34
-}
-body {
-    background-color: #282c34;
-}
-
-h1 {
-	font-family: 'Open Sans', serif;
-	font-size: 6em;
-	color: white;
-} 
-
-.btn-menu {
-	margin-left: 2rem;
-	margin-right: 2rem;
-	width: 15rem;
-	height: 15rem;
-	margin-top: 1rem;
-	margin-bottom: 1rem;
-	font-family: 'Open Sans', sans-serif;
-	font-weight: bold;
-	color: #ffffff;
-}
-.weight {
-  background:url("/Users/michaelrwalker/Documents/GitHub/hcfb-fe/src/assets");
 }
 
 </style>
---
+++ src/components/LandingMenu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="d-flex justify-content-center" style="margin-top: 3em; margin-bottom: 2em;">
+      <h1>[feed]</h1>
+    </div>
+    <div class="d-flex justify-content-center">
+      <button type="button" class="btn btn-menu"><i class="fas fa-apple-alt" style="font-size: 8em; color: #EE0055">
+      </i><br><br>
+      Food Rescue</button>
+      <button  type="button" class="btn btn-menu weight"><i class="fas fa-weight" style="font-size: 8em; color: #7DB1B1">
+      </i><br><br>
+      Warehouse Entry </button>
+    </div>
+    <div class="d-flex justify-content-center">
+      <button  type="button" class="btn btn-menu"><i class="fas fa-truck" style="font-size: 8em; color: #0e6502">
+      </i><br><br>
+      Distribution </button>
+      <button  type="button" class="btn btn-menu"><i class="fas fa-tools" style="font-size: 8em; color: #2c33CC">
+      </i><br><br>
+      Admin </button>
+    </div>      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingMenu',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body {
+    background-color: #282c34;
+}
+
+h1 {
+	font-family: 'Open Sans', serif;
+	font-size: 6em;
+	color: white;
+} 
+
+.btn-menu {
+	margin-left: 2rem;
+	margin-right: 2rem;
+	width: 15rem;
+	height: 15rem;
+	margin-top: 1rem;
+	margin-bottom: 1rem;
+	font-family: 'Open Sans', sans-serif;
+	font-weight: bold;
+	color: #ffffff;
+}
+.weight {
+  background:url("/Users/michaelrwalker/Documents/GitHub/hcfb-fe/src/assets");
+}
+</style>
